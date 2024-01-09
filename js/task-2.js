b8f67dd60b3870b64030c7526c088209044f952d
YuliiaZolotareva <juliazolotareva1998@gmail.com>
2024-01-10 00:40:11 +0100

    commit

---
+++ js/task-2.js
@@ -0,0 +1,49 @@
+function calcAverageCalories(days) {
+    let calories = [];
+    let totalCalories = 0;
+
+    
+    for (const day of days) {
+        calories.push(day.calories)
+    }
+    
+    for (let i = 0; i < calories.length; i++){
+        totalCalories += calories[i];
+    }
+
+    const averageValue = totalCalories / calories.length;
+
+    return averageValue;
+}
+
+ 
+
+
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 }
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 }
+  ])
+); // 2270
+
+console.log(
+  calcAverageCalories([])
+); // 0
